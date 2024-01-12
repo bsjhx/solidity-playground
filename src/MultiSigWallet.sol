fbe259c70669a38440d1b218868676cd281a6f19
bsjhx <rakoczymichal@hotmail.com>
2024-01-12 13:18:00 +0100

    Added comments

--- src/MultiSigWallet.sol
+++ src/MultiSigWallet.sol
@@ -5,32 +5,38 @@ import {ECDSA} from "lib/openzeppelin-contracts/contracts/utils/cryptography/ECD
 import "lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
 
 /**
- * @title MultiSigWalletTest
+ * @title MultiSigWallet
  * @author Michal Rakoczy
- * @dev A example contract to show how manage signing messages using Foundry
- * @dev Also it shows signature repay exploit (and how to fix it with nonce)
- * @notice This implementation assumes only two owners but nothing in the way to add more (mostly change requires constructor)
- * @notice Each owner can deposits any amount it wants - it doesn't require signatures
- * @notice To withdraw Eth, signed tx is required
+ * @dev A contract for managing multi-signature wallets using Foundry and addressing a signature replay exploit with nonces.
+ * @dev Owners can deposit any amount without requiring signatures. To withdraw, a signed transaction is needed.
+ * @notice This implementation supports two owners by default, but it can be extended for more owners by adjusting the constructor.
+ * @notice Each owner can deposit any amount without needing signatures.
+ * @notice To withdraw Ether, a signed transaction is required.
  */
 contract MultiSigWallet {
     using ECDSA for bytes32;
 
-    address[2] public s_owners;
-    mapping(bytes32 => bool) private s_executed;
+    address[2] public s_owners; // Array to store wallet owners
+    mapping(bytes32 => bool) private s_executed; // Mapping to track executed transactions
 
     /**
-     * @param _owners Owners required to manage wallet
+     * @param _owners Array of addresses representing the owners required to manage the wallet.
      */
     constructor(address[2] memory _owners) payable {
         s_owners = _owners;
     }
 
+    /**
+     * @dev Allows any owner to deposit Ether into the wallet.
+     */
     function deposit() external payable {}
 
     /**
-     * @notice Transfers `_amount` Eth from wallet to `_to`, requires signed tx in _sigs.
-     * @notice This is part of broken implementation - it doesn't use nonce!!!
+     * @notice Transfers `_amount` Eth from the wallet to `_to` using signed transactions in `_sigs`.
+     * @notice This is part of a broken implementation that doesn't use nonce and is susceptible to a signature replay exploit.
+     * @param _to The address to which the Ether is being transferred.
+     * @param _amount The amount of Ether to be transferred.
+     * @param _sigs Array of signatures from the wallet owners.
      */
     function transfer(
         address _to,
@@ -38,11 +44,18 @@ contract MultiSigWallet {
         bytes[2] memory _sigs
     ) external {
         bytes32 txHash = getTxHash(_to, _amount);
-        require(_checkSigs(_sigs, txHash), "invalid sig");
+        require(_checkSigs(_sigs, txHash), "Invalid signature");
         (bool sent,) = _to.call{value: _amount}("");
         require(sent, "Failed to send Ether");
     }
 
+    /**
+     * @notice Transfers `_amount` Eth from the wallet to `_to` using signed transactions in `_sigs` with a nonce.
+     * @param _to The address to which the Ether is being transferred.
+     * @param _amount The amount of Ether to be transferred.
+     * @param _nonce Nonce to prevent signature replay attacks.
+     * @param _sigs Array of signatures from the wallet owners.
+     */
     function transferOk(
         address _to,
         uint256 _amount,
@@ -50,8 +63,8 @@ contract MultiSigWallet {
         bytes[2] memory _sigs
     ) external {
         bytes32 txHash = getTxHashOk(_to, _amount, _nonce);
-        require(!s_executed[txHash], "signature already used");
-        require(_checkSigs(_sigs, txHash), "invalid sig");
+        require(!s_executed[txHash], "Signature already used");
+        require(_checkSigs(_sigs, txHash), "Invalid signature");
 
         s_executed[txHash] = true;
         (bool sent,) = _to.call{value: _amount}("");
@@ -59,7 +72,9 @@ contract MultiSigWallet {
     }
 
     /**
-     * @notice This is part of broken implementation - it doesn't use nonce!!!
+     * @notice Generates the transaction hash without a nonce.
+     * @param _to The address to which the Ether is being transferred.
+     * @param _amount The amount of Ether to be transferred.
      */
     function getTxHash(address _to, uint256 _amount)
     public
@@ -69,6 +84,12 @@ contract MultiSigWallet {
         return keccak256(abi.encodePacked(_to, _amount));
     }
 
+    /**
+     * @notice Generates the transaction hash with a nonce to prevent signature replay attacks.
+     * @param _to The address to which the Ether is being transferred.
+     * @param _amount The amount of Ether to be transferred.
+     * @param _nonce Nonce to prevent signature replay attacks.
+     */
     function getTxHashOk(address _to, uint256 _amount, uint256 _nonce)
     public
     pure
@@ -77,6 +98,12 @@ contract MultiSigWallet {
         return keccak256(abi.encodePacked(_to, _amount, _nonce));
     }
 
+    /**
+     * @dev Internal function to check the validity of multiple signatures.
+     * @param _sigs Array of signatures from the wallet owners.
+     * @param _txHash The hash of the transaction.
+     * @return A boolean indicating whether all signatures are valid.
+     */
     function _checkSigs(bytes[2] memory _sigs, bytes32 _txHash)
     private
     view
