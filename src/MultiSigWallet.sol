f7f3bb62b3e0e720d88496e8ea63f2d963012ba5
bsjhx <rakoczymichal@hotmail.com>
2024-01-12 12:51:58 +0100

    Added signing messages and tested it

--- src/MultiSigWallet.sol
+++ src/MultiSigWallet.sol
@@ -4,17 +4,33 @@ pragma solidity 0.8.22;
 import {ECDSA} from "lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 import "lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
 
+/**
+ * @title MultiSigWalletTest
+ * @author Michal Rakoczy
+ * @dev A example contract to show how manage signing messages using Foundry
+ * @dev Also it shows signature repay exploit (and how to fix it with nonce)
+ * @notice This implementation assumes only two owners but nothing in the way to add more (mostly change requires constructor)
+ * @notice Each owner can deposits any amount it wants - it doesn't require signatures
+ * @notice To withdraw Eth, signed tx is required
+ */
 contract MultiSigWallet {
     using ECDSA for bytes32;
 
-    address[2] public owners;
+    address[2] public s_owners;
 
+    /**
+     * @param _owners Owners required to manage wallet
+     */
     constructor(address[2] memory _owners) payable {
-        owners = _owners;
+        s_owners = _owners;
     }
 
     function deposit() external payable {}
 
+    /**
+     * @notice Transfers `_amount` Eth from wallet to `_to`, requires signed tx in _sigs.
+     * @notice This is part of broken implementation - it doesn't use nonce!!!
+     */
     function transfer(
         address _to,
         uint256 _amount,
@@ -22,13 +38,16 @@ contract MultiSigWallet {
     ) external {
         bytes32 txHash = getTxHash(_to, _amount);
         require(_checkSigs(_sigs, txHash), "invalid sig");
-        (bool sent, ) = _to.call{value: _amount}("");
+        (bool sent,) = _to.call{value: _amount}("");
         require(sent, "Failed to send Ether");
     }
 
+    /**
+     * @notice This is part of broken implementation - it doesn't use nonce!!!
+     */
     function getTxHash(address _to, uint256 _amount)
     public
-    view
+    pure
     returns (bytes32)
     {
         return keccak256(abi.encodePacked(_to, _amount));
@@ -42,7 +61,7 @@ contract MultiSigWallet {
         bytes32 ethSignedHash = MessageHashUtils.toEthSignedMessageHash(_txHash);
         for (uint256 i = 0; i < _sigs.length; i++) {
             address signer = ethSignedHash.recover(_sigs[i]);
-            bool valid = signer == owners[i];
+            bool valid = signer == s_owners[i];
             if (!valid) {
                 return false;
             }
---
+++ test/MultiSigWallet.t.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
+
+import "forge-std/Test.sol";
+import "../src/MultiSigWallet.sol";
+import "lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+
+// https://book.getfoundry.sh/cheatcodes/sign
+contract MultiSigWalletTest is Test {
+    using ECDSA for bytes32;
+
+    MultiSigWallet private wallet;
+    address private evil = address(666);
+
+    address private aliceAddress;
+    uint256 private alicePrivateKey;
+    address private bobAddress;
+    uint256 private bobPrivateKey;
+
+    function setUp() public {
+        (aliceAddress, alicePrivateKey) = makeAddrAndKey("alice");
+        (bobAddress, bobPrivateKey) = makeAddrAndKey("bob");
+
+        wallet = new MultiSigWallet([aliceAddress, bobAddress]);
+        vm.deal(aliceAddress, 100 ether);
+        vm.deal(bobAddress, 100 ether);
+
+    }
+
+    function test_basic() public depositedEth {
+        assertEq(address(wallet).balance, 20 ether);
+        assertEq(evil.balance, 0 ether);
+
+        // 1. Get tx hash
+        vm.prank(aliceAddress);
+        bytes32 txHash = wallet.getTxHash(evil, 1 ether);
+
+        // 2. Alice signs tx hash
+        bytes32 digest = MessageHashUtils.toEthSignedMessageHash(txHash);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(alicePrivateKey, digest);
+        bytes memory aliceSignedTxHash = abi.encodePacked(r, s, v);
+
+        // 2. Bob signs tx hash
+        (uint8 v2, bytes32 r2, bytes32 s2) = vm.sign(bobPrivateKey, digest);
+        bytes memory bobSignedTxHash = abi.encodePacked(r2, s2, v2);
+
+        // 3. Evil withdraw Eth
+        vm.startPrank(evil);
+        wallet.transfer(evil, 1 ether, [aliceSignedTxHash, bobSignedTxHash]);
+
+        // 4. ... and does it again with same signed message
+        wallet.transfer(evil, 1 ether, [aliceSignedTxHash, bobSignedTxHash]);
+
+        vm.stopPrank();
+
+        assertEq(address(wallet).balance, 18 ether);
+        assertEq(evil.balance, 2 ether);
+
+    }
+
+    modifier depositedEth {
+        vm.prank(aliceAddress);
+        wallet.deposit{value: 10 ether}();
+
+        vm.prank(bobAddress);
+        wallet.deposit{value: 10 ether}();
+
+        _;
+    }
+}
